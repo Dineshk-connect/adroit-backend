b6cb512d8bcd11e7c54e28f6362c50223778d992
Dineshk-connect <dinesh.k.connect@gmail.com>
2026-02-03 21:07:32 +0530

    Fix CORS for Vercel domain

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const { Resend } = require('resend');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
-app.use(cors({
-  origin: "*",
-}));
+app.use(
+  cors({
+    origin: ["https://adroit-website-five.vercel.app/"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  }),
+);
+
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log("MongoDB Connected"));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"));
 
 const EnquirySchema = new mongoose.Schema({
   firstName: String,
@@ -22,15 +28,15 @@ const EnquirySchema = new mongoose.Schema({
   message: String,
 });
 
-const Enquiry = mongoose.model('Enquiry', EnquirySchema);
+const Enquiry = mongoose.model("Enquiry", EnquirySchema);
 
-app.post('/enquiry', async (req, res) => {
+app.post("/enquiry", async (req, res) => {
   const data = req.body;
 
   await Enquiry.create(data);
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -38,11 +44,11 @@ app.post('/enquiry', async (req, res) => {
   });
 
   await resend.emails.send({
-  from: 'Adroit Website <onboarding@resend.dev>',
-  to: process.env.COMPANY_EMAIL,
-  reply_to: data.email,
-  subject: 'New Enquiry from Website',
-  html: `
+    from: "Adroit Website <onboarding@resend.dev>",
+    to: process.env.COMPANY_EMAIL,
+    reply_to: data.email,
+    subject: "New Enquiry from Website",
+    html: `
     <h2>New Website Enquiry</h2>
     <p><b>Name:</b> ${data.firstName} ${data.lastName}</p>
     <p><b>Email:</b> ${data.email}</p>
@@ -50,12 +56,9 @@ app.post('/enquiry', async (req, res) => {
     <p><b>Message:</b></p>
     <p>${data.message}</p>
   `,
-});
-
-
+  });
 
   res.json({ message: "Enquiry sent successfully" });
-
 });
 
 app.listen(5000, () => console.log("Server running on port 5000"));
